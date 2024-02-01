cdab3065504888b4768ff10349aa35680bc5430e
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2024-02-01 09:18:59 +0700

    feat : [AS] calculate aritmatik

--- app/src/main/java/com/example/latihanandroid10pplg2/KalkulatorAritmatik.java
+++ app/src/main/java/com/example/latihanandroid10pplg2/KalkulatorAritmatik.java
@@ -3,6 +3,7 @@ package com.example.latihanandroid10pplg2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -24,5 +25,44 @@ public class KalkulatorAritmatik extends AppCompatActivity {
         txtAngka1 = findViewById(R.id.txtAngkaSatu);
         txtAngka2 = findViewById(R.id.txtAngkaDua);
         tvHasil = findViewById(R.id.tvHasil);
+        spOperator = findViewById(R.id.spOperator);
+        btnHitung = findViewById(R.id.btnHitung);
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action kalkulasi angka1 dan 2
+                int angka1 = Integer.parseInt(txtAngka1.getText().toString());
+                int angka2 = Integer.parseInt(txtAngka2.getText().toString());
+                int hasil = 0;
+                String text = spOperator.getSelectedItem().toString();
+                switch (text) {
+                    case "Tambah":
+                        hasil = angka1 + angka2;
+                        tvHasil.setText(hasil + "");
+                        break;
+
+                    case "Kurang":
+                        hasil = angka1 - angka2;
+                        tvHasil.setText(hasil + "");
+                        break;
+
+                    case "Kali":
+                        hasil = angka1 * angka2;
+                        tvHasil.setText(hasil + "");
+                        break;
+
+                    case "Bagi":
+                        double angka1double = Double.parseDouble(txtAngka1.getText().toString());
+                        double angka2double = Double.parseDouble(txtAngka2.getText().toString());
+                        double hasilDouble = angka1double/angka2double;
+                        tvHasil.setText(hasilDouble+"");
+                        break;
+
+
+                }
+
+
+            }
+        });
     }
 }
